0951fddafd85168e1b9284db25dd25b639eadf18
danielasun <sundaniel3@gmail.com>
2023-02-18 11:17:59 -0800

    add Gogh submodule and have it install nord theme, leaving others to be installed separately if desired

--- install_dev_tools.sh
+++ install_dev_tools.sh
@@ -5,3 +5,12 @@ sudo apt install git vim fonts-powerline
 # installs locally installed fonts
 sudo fc-cache -vf ~/.local/share/fonts
 
+
+# gogh dependencies
+sudo apt-get install dconf-cli uuid-runtime
+
+git submodule update --init --recursive
+
+# install nord theme 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+${SCRIPT_DIR}/Gogh/themes/nord.sh
