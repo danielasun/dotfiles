2a1c111f284e8f0d17ce3f90c218cd0c7aa266d9
danielasun <sundaniel3@gmail.com>
2023-02-18 17:07:16 -0800

    reorganize install_dev_tools.sh and comment it better

--- install_dev_tools.sh
+++ install_dev_tools.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 sudo apt update
-sudo apt install git vim fonts-powerline
+sudo apt install git vim
 
-# installs locally installed fonts
-sudo fc-cache -vf ~/.local/share/fonts
-
-
-# gogh dependencies
-sudo apt-get install dconf-cli uuid-runtime
+sudo snap install sublime-text --classic
+sudo snap install sublime-merge --classic
 
 git submodule update --init --recursive
+
+# tmux: 
+sudo apt install fonts-powerline
+sudo fc-cache -vf ~/.local/share/fonts  # installs locally installed fonts
+
+
+# gnome-terminal:
+sudo apt-get install dconf-cli uuid-runtime  # gogh dependencies
+
 
 # install nord theme 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ${SCRIPT_DIR}/Gogh/themes/nord.sh
+
