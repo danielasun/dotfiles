efb9ac5f66c98057c3145ad107994231607ee4a1
Daniel Sun <daniel.sun@offworld.ai>
2023-03-23 11:26:17 -0700

    copy sublime settings

--- configure_sublime_text.sh
+++ configure_sublime_text.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cp ${SCRIPT_DIR}/Preferences.sublime-settings /home/${USER}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+mkdir -p /home/${USER}/.config/sublime-text/Packages/User/
+cp -r ${SCRIPT_DIR}/Preferences.sublime-settings /home/${USER}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
