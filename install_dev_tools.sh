de1a85e4daaaea1bf10d56710056b6a9182bbdcf
Daniel Sun <daniel.sun@offworld.ai>
2023-03-23 11:27:43 -0700

    remove sudo from install_dev_tools, install python and virtualenvironment stuff and xsel so tmux works

--- install_dev_tools.sh
+++ install_dev_tools.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-sudo apt update
-sudo apt install git vim
+apt update
+apt install git vim tmux
 
-sudo snap install sublime-text --classic
-sudo snap install sublime-merge --classic
+# python, pip and virtual environment
+apt install python3-dev python3-pip virtualenv virtualenvwrapper
+
+snap install sublime-text --classic
+snap install sublime-merge --classic
+snap install chromium
+snap install code --classic
+
 
 git submodule update --init --recursive
 
 # tmux: 
-sudo apt install fonts-powerline
-sudo fc-cache -vf ~/.local/share/fonts  # installs locally installed fonts
+apt install fonts-powerline xsel
+fc-cache -vf ~/.local/share/fonts  # installs locally installed fonts
 
 
 # gnome-terminal:
-sudo apt-get install dconf-cli uuid-runtime  # gogh dependencies
+apt-get install dconf-cli uuid-runtime  # gogh dependencies
 
 
 # install nord theme 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-${SCRIPT_DIR}/Gogh/themes/nord.sh
-
+export TERMINAL=gnome-terminal
