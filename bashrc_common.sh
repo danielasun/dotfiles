dddd125951dd277cb7849313b398e920f1cbbf9a
Daniel Sun <daniel.sun@offworld.ai>
2023-03-28 14:35:09 -0700

    add file for common bashrc

---
+++ bashrc_common.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# virtualenvwrapper
+export VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
