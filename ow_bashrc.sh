bf9de00f57d209ae3aabc41e14d7993cc26a0edc
Daniel Sun <daniel.sun@offworld.ai>
2023-03-28 14:34:47 -0700

    add separate files for offworld bash

---
+++ ow_bashrc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ow_bash.sh
+
+# ROS specific
+export ROS_DOMAIN_ID=100
+
+source /opt/ros/noetic/setup.bash
+# source /opt/ros/galactic/setup.bash
+
+
+export PYTHONPATH=$PYTHONPATH:/home/offworld/swarm-robotic-mining/dev_ws/src
+export OFFWORLD_HOME=/home/offworld
+. "$HOME/.cargo/env"
---
+++ ow_excavator.sh
@@ -0,0 +1,12 @@
+source ~/ve/py38/bin/activate
+export OFFWORLD_ROOT=/home/dsun/offworld
+export OFFWORLD_HOME=/home/dsun
+source /opt/ros/noetic/setup.bash
+
+# SRM Path
+export PYTHONPATH=/opt/ros/noetic/lib/python3/dist-packages:/home/offworld/swarm-robotic-mining/dev_ws/src:/home/offworld/swarm-robotic-mining/dev_ws/src:/home/dsun/swarm-robotic-mining/dev_ws/src:/home/dsun/swarm-robotic-mining/dev_ws/src/firmware/bot_firmware_apis:/home/dsun/swarm-robotic-mining/dev_ws/src/bot_mobilebase_api/build:/home/dsun/swarm-robotic-mining/dev_ws/src/bot_overseer_api:/home/dsun/swarm-robotic-mining/dev_ws/src/bot_database:/home/dsun/swarm-robotic-mining/dev_ws/src/bot_derived_tm:/home/dsun/swarm-robotic-mining/dev_ws/src/bot_events:/home/dsun/swarm-robotic-mining/dev_ws/src/offworld-ur-interface:/home/dsun/offworld/swarm-robotic-mining/dev_ws/src:/home/dsun/offworld/swarm-robotic-mining/dev_ws/src/firmware/bot_firmware_apis:/home/dsun/offworld/swarm-robotic-mining/dev_ws/src/bot_mobilebase_api/build:/home/dsun/offworld/swarm-robotic-mining/dev_ws/src/bot_overseer_api:/home/dsun/offworld/swarm-robotic-mining/dev_ws/src/bot_database:/home/dsun/offworld/swarm-robotic-mining/dev_ws/src/bot_derived_tm:/home/dsun/offworld/swarm-robotic-mining/dev_ws/src/bot_events:/home/dsun/offworld/swarm-robotic-mining/dev_ws/src/offworld-ur-interface
+export GAZEBO_MODEL_PATH=/home/dsun/swarm-robotic-mining/catkin_ws/src/bot_digger_gazebo/models:/home/dsun/swarm-robotic-mining/catkin_ws/src/bot_digger_gazebo/models:
+# ROS
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_HOSTNAME=localhost
+source /home/dsun/offworld/catkin_ws/devel/setup.bash --extend
