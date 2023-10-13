e49e7cbfd7e741e8436eb98a1fd7a3579277c68c
Daniel Sun <sundaniel3@gmail.com>
2023-10-13 12:57:11 -0700

    configure installing ROS

--- install_ros.sh
+++ install_ros.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-sudo apt install curl # if you haven't already installed curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-
 sudo apt update
 sudo apt install ros-noetic-desktop-full
+sudo apt install ros-galactic-desktop-full
