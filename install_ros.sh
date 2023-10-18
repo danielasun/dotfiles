1697068b9b27e6a24bbefb78777e8c8715e92661
Daniel Sun <sundaniel3@gmail.com>
2023-10-18 14:01:23 -0700

    use different repository key for installing ROS

--- install_ros.sh
+++ install_ros.sh
@@ -3,9 +3,12 @@ sudo apt install software-properties-common
 sudo add-apt-repository universe
 
 sudo apt update && sudo apt install curl
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+# sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+# add new repository key and remove old repo key:
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+# sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116 # remove old repository key:
 
 sudo apt update
 sudo apt install ros-noetic-desktop-full
