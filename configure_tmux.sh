f6b32dbac37741a7659e43d18b27cab38d276b7c
Daniel Sun <sundaniel3@gmail.com>
2023-10-27 18:10:50 -0700

    add fonts-powerline to tmux

--- configure_tmux.sh
+++ configure_tmux.sh
@@ -1,2 +1,3 @@
 sudo apt install xsel # makes tmux-yank work
+sudo apt-get install fonts-powerline # makes the pane fonts look correct
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
