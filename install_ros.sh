bb9b8d17aba716cf7b5637506d997f6c8d762a3b
Daniel Sun <sundaniel3@gmail.com>
2023-10-27 18:11:06 -0700

    fix the ros install, hopefully

--- install_ros.sh
+++ install_ros.sh
@@ -2,14 +2,15 @@
 sudo apt install software-properties-common
 sudo add-apt-repository universe
 
-sudo apt update && sudo apt install curl
-# sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt update && sudo apt install curl gnupg lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
 
 # add new repository key and remove old repo key:
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-# sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116 # remove old repository key:
+# sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 # TODO: remove if this continues to work
+# sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116 # remove old repository key: # TODO: remove if this continues to work
 
 sudo apt update
-sudo apt install ros-noetic-desktop-full
-sudo apt install ros-galactic-desktop-full
+sudo apt install ros-noetic-desktop
+sudo apt install ros-galactic-desktop
