6dfe4de31f9f7ac49a3b3e123c9e1dcb9af3685c
Daniel Sun <sundaniel3@gmail.com>
2023-10-27 18:11:27 -0700

    delete the cargo env part of the ow_surveyor

--- ow_surveyor.sh
+++ ow_surveyor.sh
@@ -12,6 +12,5 @@ source /opt/ros/galactic/setup.bash
 OFFWORLD_HOME="/home/offworld"
 export PYTHONPATH=$PYTHONPATH:/home/offworld/swarm-robotic-mining/dev_ws/src
 export OFFWORLD_HOME=${OFFWORLD_HOME}
-. "$HOME/.cargo/env"
 
 export IGN_GAZEBO_RESOURCE_PATH=${OFFWORLD_HOME}/catkin_ws/src:${OFFWORLD_HOME}/catkin_ws/src/motoman_ow:${OFFWORLD_HOME}/catkin_ws/src/universal_robot:$IGN_GAZEBO_RESOURCE_PATH
