8975c2af14af62140a5cd91883270453eb4f7789
Daniel Sun <sundaniel3@gmail.com>
2023-10-27 17:14:28 -0700

    update install_dev_tools to use sudo and -y for apt installs

--- install_dev_tools.sh
+++ install_dev_tools.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-apt update
-apt install git vim tmux curl
+sudo apt update
+sudo apt install -y git vim tmux curl
 
 # python, pip and virtual environment
-apt install python3-dev python3-pip virtualenv virtualenvwrapper
+sudo apt install -y python3-dev python3-pip
 
-snap install sublime-text --classic
-snap install sublime-merge --classic
-snap install chromium
-snap install code --classic
+sudo snap install sublime-text --classic
+sudo snap install sublime-merge --classic
+sudo snap install chromium
+sudo snap install code --classic
 
 
 git submodule update --init --recursive
@@ -19,7 +19,7 @@ fc-cache -vf ~/.local/share/fonts  # installs locally installed fonts
 
 
 # gnome-terminal:
-apt-get install dconf-cli uuid-runtime  # gogh dependencies
+sudo apt-get install dconf-cli uuid-runtime  # gogh dependencies
 
 
 # install nord theme 
