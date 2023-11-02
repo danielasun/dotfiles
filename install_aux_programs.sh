da91eb498881744073e1e88aba7cd45dd7811e90
Daniel Sun <sundaniel3@gmail.com>
2023-11-02 15:22:16 -0700

    add zotero to aux programs installed

--- install_aux_programs.sh
+++ install_aux_programs.sh
@@ -4,4 +4,9 @@ sudo apt install -y gnome-tweaks
 sudo snap install spotify
 sudo snap install plotjuggler
 sudo snap set plotjuggler ros-plugin-version=2
-sudo snap install zotero-snap	
+sudo snap install zotero-snap
+
+# no machine download and install
+curl https://download.nomachine.com/download/8.9/Linux/nomachine_8.9.1_1_amd64.deb -o nomachine.deb
+sudo apt install ./nomachine.deb
+rm ./nomachine.deb
